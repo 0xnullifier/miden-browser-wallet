78ccb24dc3c1df0508fe2d070243c625fc7fb20f
utkarshdagoat <utkarsh382004@gmail.com>
2025-07-24 21:14:54 +0530

    chore: update websocket url to the hosted signaling server

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-background text-foreground font-mono flex justify-center items-center">
+    <div className="bg-background text-foreground font-mono flex justify-center items-center px-4">
       Try out the web wallet by clicking wallet on the top
     </div>
   )
