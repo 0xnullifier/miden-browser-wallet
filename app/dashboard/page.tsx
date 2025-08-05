b3534bc32521110cae5e0f8f588fcc9bd1119f58
0xnullifier <utkarsh382004@gmail.com>
2025-08-05 20:20:00 +0530

    feat: dashboard init

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-
+import { Dashboard as D } from "@/components/dashboard/dashboard";
 export default function Dashboard() {
     return (
-        <div className="flex justify-center items-center text-2xl font-light" >
-            Coming Soon
-        </div>
+        <D />
     )
 }
 
---
+++ app/dashboard/tx/[id]/page.tsx
@@ -0,0 +1,6 @@
+import { TransactionDetails } from '@/components/dashboard/tx-detail'
+export default function TransactionDetailPage() {
+    return (
+        <TransactionDetails />
+    );
+}
